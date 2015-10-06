cdc9c7a0da834d6dd64ede44f78f7dc8eaf23fca
GioBakradze <gio.bakradze.92@gmail.com>
2015-10-06 18:12:07 +0400

    first steps

--- app/bundle.js
+++ app/bundle.js
@@ -52,7 +52,87 @@
 
 	var _three2 = _interopRequireDefault(_three);
 
-	console.log('webpack in actiiiooon');
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+
+	var renderer = new _three2['default'].WebGLRenderer({
+	    antialias: true
+	});
+	renderer.setSize(width, height);
+	document.body.appendChild(renderer.domElement);
+
+	var scene = new _three2['default'].Scene();
+
+	// create cue
+	var cubeGeometry = new _three2['default'].CubeGeometry(100, 100, 100);
+	var cubeMaterial = new _three2['default'].MeshLambertMaterial({
+	    color: 0x1ec876
+	});
+	var cube = new _three2['default'].Mesh(cubeGeometry, cubeMaterial);
+	cube.rotation.y = Math.PI * 45 / 180;
+	scene.add(cube);
+
+	// create camera
+	var camera = new _three2['default'].PerspectiveCamera(45, width / height, 0.1, 10000);
+	camera.position.x = 0;
+	camera.position.y = 0;
+	camera.position.z = 200;
+	scene.add(camera);
+
+	// create skybox
+	var skyboxGeometry = new _three2['default'].CubeGeometry(10000, 10000, 10000);
+	var skyboxMaterial = new _three2['default'].MeshBasicMaterial({
+	    color: 0x000000,
+	    side: _three2['default'].BackSide
+	});
+	var skybox = new _three2['default'].Mesh(skyboxGeometry, skyboxMaterial);
+	scene.add(skybox);
+
+	// create light
+	var pointLight = new _three2['default'].PointLight(0xffffff);
+	pointLight.position.set(0, 0, 200);
+	scene.add(pointLight);
+
+	// create sphere
+	// var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+	// var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+	// var sphere = new THREE.Mesh( geometry, material );
+	// sphere.position.x =
+	// scene.add( sphere );
+
+	camera.lookAt(cube.position);
+	// renderer.render(scene, camera);
+
+	var clock = new _three2['default'].Clock();
+	var pos = 0;
+	var posX = 0;
+	function render() {
+	    renderer.render(scene, camera);
+	    // cube.rotation.y -= clock.getDelta();
+	    // cube.position.y = pos;
+	    // cube.position.x = posX;
+
+	    camera.position.x = posX;
+	    camera.position.y = pos;
+
+	    pointLight.position.x = posX;
+	    pointLight.position.y = pos;
+	    camera.lookAt(cube.position);
+
+	    window.requestAnimationFrame(render);
+	}
+
+	var next = null;
+	document.addEventListener('mousemove', function (e) {
+	    if (!!next) {
+	        pos = e.clientX - next > 0 ? pos + 1 : pos - 1;
+	        posX = e.clientX - next > 0 ? pos + 1 : pos - 1;
+	        console.log(pos);
+	    }
+	    next = e.clientX;
+	}, false);
+
+	render();
 
 /***/ },
 /* 1 */
