d0842bbf012387e9cf4487871aba81276914c2f2
GioBakradze <gio.bakradze.92@gmail.com>
2015-10-13 12:07:30 +0400

    all game elemetns are on the board

--- app/js/board.js
+++ app/js/board.js
@@ -2,19 +2,56 @@ import message from './message';
 
 function Board() {
 
-    var boardHeight = 144;
-    var boardWidth = 76;
+    var boardHeight = 100;
+    var boardWidth = 74; // 76
     var borderWidth = 2;
+    var blocksRow = 5;
+    var blocksCol = 10;
 
-    this.board = Array.apply(null, Array(boardHeight)).map(() => Array.apply(null, Array(boardWidth)).map(() => 0));
+    // this.board = Array.apply(null, Array(boardHeight)).map(() => Array.apply(null, Array(boardWidth)).map(() => 0));
     // create borders
-    this.board = this.board.map((e, i) => e.map( (e2, i2) => ( (i2 <= -1 + borderWidth || i2 >= e.length - borderWidth) || (i <= -1 + borderWidth || i >= this.board.length - borderWidth) ? 1 : 0 )));
+    // this.board = this.board.map((e, i) => e.map( (e2, i2) => ( (i2 <= -1 + borderWidth || i2 >= e.length - borderWidth) || (i <= -1 + borderWidth || i >= this.board.length - borderWidth) ? 1 : 0 )));
+
+    // the block size is 5x2 space between
+    // blocks is 2 units, keep that in mind, also border is 2 units
+    // when trying to understand logic behind those cycles
+    this.blocks = [];
+    for (let i=0; i < blocksRow; i++) {
+        let startX, startY;
+        let x, y;
+        // startY = boardHeight/2 + blocksRow * 4;
+        startY = boardHeight/2 - blocksRow * 4;
+        startX = -1 * boardWidth / 2 + 5.5;
+
+        y = startY + 4*i;
+        for (var j=0; j < blocksCol; j++) {
+            x = startX + 7*j;
+            this.blocks.push({
+                x: x,
+                y: y,
+                width: 5,
+                height: 2
+            });
+        }
+    }
 
     this.borders = [
         {x: -1* (boardWidth/2), y: 0, width: borderWidth, height: boardHeight},
         {x: 0, y: -1*(boardHeight/2), width: boardWidth, height: borderWidth},
         {x: 0, y: boardHeight/2, width: boardWidth, height: borderWidth},
         {x: boardWidth/2, y: 0, width: borderWidth, height: boardHeight},
+        // {x: 0, y: 0, width: borderWidth, height: boardHeight},
+        // {x: 0, y: 0, width: boardWidth, height: borderWidth},
+        // {x: 0, y: boardHeight/2, width: boardWidth, height: borderWidth},
+        // {x: boardWidth/2, y: 0, width: borderWidth, height: boardHeight},
+    ];
+
+    this.user = [
+        {x: 0, y: -1*(boardHeight/2) + 3, width: 10, height: 2}
+    ];
+
+    this.ball = [
+        {x: 0, y: -1*(boardHeight/2) + 3 + 1.5 + 1, width: 3, height: 3}
     ];
 }
 
--- app/js/view.js
+++ app/js/view.js
@@ -1,78 +1,88 @@
 import THREE from 'three';
 import board from './board';
 
-var renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    alpha: true
-});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+function View(b) {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
 
-var scene = new THREE.Scene();
-var center = new THREE.Vector3( 0, 0, 0 );
-
-// ########################################
-// create point light like a light bulb
-var light = new THREE.PointLight(0xffffff);
-light.position.set( 50, 50, 250 );
-scene.add( light );
-
-// ########################################
-// create moving ball
-var cubeGeometry = new THREE.CubeGeometry(5, 5, 5);
-var cubeMaterial = new THREE.MeshPhongMaterial({
-    color: 0x1ec876
-});
-var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cube);
-
-// ########################################
-// create borders
-for (var i in board.borders) {
-    var b = board.borders[i];
-    var geometry = new THREE.BoxGeometry(b.width, b.height, 1);
-    var material = new THREE.MeshPhongMaterial({
-        color: 0x00afff
+    this.renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
     });
-    var cube = new THREE.Mesh(geometry, material);
-    cube.position.x = b.x;
-    cube.position.y = b.y;
-    cube.position.z = 0;
-    scene.add(cube);
-}
+    this.renderer.setSize(this.width, this.height);
+    document.body.appendChild(this.renderer.domElement);
 
-// ########################################
-// create camera
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 200;
-scene.add(camera);
+    this.scene = new THREE.Scene();
+    this.center = new THREE.Vector3( 0, 0, 0 );
 
+    // ########################################
+    // create point light like a light bulb
+    this.light = new THREE.PointLight(0xffffff);
+    this.light.position.set( 50, 50, 250 );
+    this.scene.add(this.light);
 
-// ########################################
-// some random events
-camera.lookAt(center);
-var clock = new THREE.Clock();
-var pos = 0;
-var posX = 0;
+    // ########################################
+    // create moving ball
+    // this.cubeGeometry = new THREE.CubeGeometry(5, 5, 2);
+    // this.cubeMaterial = new THREE.MeshPhongMaterial({
+    //     color: 0x1ec876
+    // });
+    // this.cube = new THREE.Mesh(this.cubeGeometry, this.cubeMaterial);
+    // this.scene.add(this.cube);
 
-function render() {
-    renderer.render(scene, camera);
-    camera.position.x = posX;
-    camera.position.y = pos;
-    camera.lookAt(center);
-    window.requestAnimationFrame(render);
-}
+    // ########################################
+    // create camera
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+    this.camera.position.x = 0;
+    this.camera.position.y = 0;
+    this.camera.position.z = 200;
+    this.scene.add(this.camera);
 
-var next = null;
-document.addEventListener('mousemove', function(e) {
-    if (!!next) {
-        pos = e.clientX - next > 0 ? pos + 1 : pos - 1;
-        posX = e.clientX - next > 0 ? pos + 1 : pos - 1;
-        // console.log(pos);
+    this.dropBlocks(b.borders, 0x00afff);
+    this.dropBlocks(b.blocks, 0xf0676f);
+    this.dropBlocks(b.user, 0x1ec876);
+    this.dropBlocks(b.ball, 0xEFD76F);
+
+    // ########################################
+    // some random events
+    this.camera.lookAt(this.center);
+    var clock = new THREE.Clock();
+    var pos = 0;
+    var posX = 0;
+
+    function render() {
+        this.renderer.render(this.scene, this.camera);
+        this.camera.position.x = posX;
+        this.camera.position.y = pos;
+        this.camera.lookAt(this.center);
+        window.requestAnimationFrame(render.bind(this));
     }
-    next = e.clientX;
-}, false);
 
-render();
+    var next = null;
+    document.addEventListener('mousemove', function(e) {
+        if (!!next) {
+            pos = e.clientX - next > 0 ? pos + 1 : pos - 1;
+            posX = e.clientX - next > 0 ? pos + 1 : pos - 1;
+        }
+        next = e.clientX;
+    }, false);
+
+    render.bind(this)();
+}
+
+View.prototype.dropBlocks = function(array, color) {
+    for (let i in array) {
+        var b = array[i];
+        var geometry = new THREE.BoxGeometry(b.width, b.height, 1);
+        var material = new THREE.MeshPhongMaterial({
+            color: color
+        });
+        var cube = new THREE.Mesh(geometry, material);
+        cube.position.x = b.x;
+        cube.position.y = b.y;
+        cube.position.z = 0;
+        this.scene.add(cube);
+    }
+};
+
+var view = new View(board);
