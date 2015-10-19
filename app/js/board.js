c1f76259e56c76dee7007245f3fb994d5e06d553
GioBakradze <gio.bakradze.92@gmail.com>
2015-10-19 15:15:00 +0400

    added some math functions

--- app/js/board.js
+++ app/js/board.js
@@ -1,4 +1,5 @@
 import SimpleMath from './simpleMath';
+import _ from 'underscore';
 
 function Board() {
 
@@ -14,15 +15,15 @@ function Board() {
     // blocks is 2 units, keep that in mind, also border is 2 units
     // when trying to understand logic behind those cycles
     this.blocks = [];
-    for (let i=0; i < blocksRow; i++) {
+    for (let i = 0; i < blocksRow; i++) {
         let startX, startY;
         let x, y;
-        startY = boardHeight/2 - blocksRow * 4;
+        startY = boardHeight / 2 - blocksRow * 4;
         startX = -1 * boardWidth / 2 + 5.5;
 
-        y = startY + 4*i;
-        for (var j=0; j < blocksCol; j++) {
-            x = startX + 7*j;
+        y = startY + 4 * i;
+        for (var j = 0; j < blocksCol; j++) {
+            x = startX + 7 * j;
             this.blocks.push({
                 x: x,
                 y: y,
@@ -32,60 +33,157 @@ function Board() {
         }
     }
 
-    this.borders = [
-        {x: -1* (boardWidth/2), y: 0, width: borderWidth, height: boardHeight},
-        {x: 0, y: -1*(boardHeight/2), width: boardWidth, height: borderWidth},
-        {x: 0, y: boardHeight/2, width: boardWidth, height: borderWidth},
-        {x: boardWidth/2, y: 0, width: borderWidth, height: boardHeight},
-    ];
+    this.borders = [{
+        x: -1 * (boardWidth / 2),
+        y: 0,
+        width: borderWidth,
+        height: boardHeight
+    }, {
+        x: 0,
+        y: -1 * (boardHeight / 2),
+        width: boardWidth,
+        height: borderWidth
+    }, {
+        x: 0,
+        y: boardHeight / 2,
+        width: boardWidth,
+        height: borderWidth
+    }, {
+        x: boardWidth / 2,
+        y: 0,
+        width: borderWidth,
+        height: boardHeight
+    }, ];
 
-    this.user = [
-        {x: 0, y: -1*(boardHeight/2) + 3, width: 10, height: 2}
-    ];
+    this.user = [{
+        x: 0,
+        y: -1 * (boardHeight / 2) + 3,
+        width: 10,
+        height: 2
+    }];
 
-    this.ball = [
-        {x: 0, y: -1*(boardHeight/2) + 3 + 1.5 + 1, width: 3, height: 3}
-    ];
+    this.ball = [{
+        x: 0,
+        y: -1 * (boardHeight / 2) + 3 + 1.5 + 1,
+        width: 3,
+        height: 3
+    }];
 
-    this.leftSide = -1* (boardWidth/2);
-    this.rightSide = boardWidth/2;
+    this.leftSide = -1 * (boardWidth / 2);
+    this.rightSide = boardWidth / 2;
+    this.topSide = boardHeight / 2;
+    this.bottomSide = -1 * (boardHeight / 2);
 
     this.ballRightSide = this.rightSide - 2.5;
     this.ballLeftSide = this.leftSide + 2.5;
 
+    this.halfBall = 2.5;
+
+    this.ballCollisionSegments = [
+        // vertical line segments
+        [{
+            x: this.leftSide + this.halfBall,
+            y: this.bottomSide + this.halfBall
+        }, {
+            x: this.leftSide + this.halfBall,
+            y: this.topSide - this.halfBall
+        }],
+        [{
+            x: this.rightSide - this.halfBall,
+            y: this.bottomSide + this.halfBall
+        }, {
+            x: this.rightSide - this.halfBall,
+            y: this.topSide - this.halfBall
+        }],
+
+        // horizontal line segments
+        [{
+            x: this.leftSide + this.halfBall,
+            y: this.topSide - this.halfBall
+        }, {
+            x: this.rightSide - this.halfBall,
+            y: this.topSide - this.halfBall
+        }],
+        [{
+            x: this.leftSide + this.halfBall,
+            y: this.bottomSide + this.halfBall
+        }, {
+            x: this.rightSide - this.halfBall,
+            y: this.bottomSide + this.halfBall
+        }]
+    ];
+
     // game flow variables
     this.angle = Math.floor(Math.random() * 45) + 30; // get starting angle randomly between 30 and 45 degrees
+    this.moveCount = 1;
 }
 
-Board.prototype.movePadLeft = function () {
+Board.prototype.movePadLeft = function() {
     var curX = this.user[0].x;
     this.user[0].x = curX - 2 >= this.leftSide + 5 + 1 ? curX - 2 : curX;
 };
 
-Board.prototype.movePadRight = function () {
+Board.prototype.movePadRight = function() {
     var curX = this.user[0].x;
     this.user[0].x = curX + 2 <= this.rightSide - 5 - 1 ? curX + 2 : curX;
 };
 
-Board.prototype.getNextMove = function () {
+Board.prototype.getNextMove = function() {
 
-    var x = (this.ball[0].x === this.ballLeftSide) ? this.ballRightSide : this.ballLeftSide;
+    var currentX = this.ball[0].x;
+    var currentY = this.ball[0].y;
 
-    var currentAngle = 90 - this.angle;
-    // currentAngle = x < this.ball[0].x ? 180 - currentAngle : currentAngle;
-    this.angle = currentAngle;
-    currentAngle = x < this.ball[0].x ? 90 +  currentAngle : currentAngle;
+    // new coordinates
+    var destX, destY;
 
+    // generate some large segment
+    var currentAngle = !(this.moveCount & 1) ? 180 - this.angle : this.angle;
     currentAngle = Math.tan(SimpleMath.toRadians(currentAngle));
 
-    // remember y = kx + b from 6th grade?
-    var y = x * currentAngle + (-1*(this.boardHeight/2) + 5.5);
+    var f = SimpleMath.getF(currentAngle, currentY);
+    var segment = {
+        a: {
+            x: this.ballRightSide,
+            y: f(this.ballRightSide)
+        },
+        b: {
+            x: this.ballLeftSide,
+            y: f(this.ballLeftSide)
+        }
+    };
 
-    console.log(this.ball[0].x, this.ballLeftSide, y);
+    var x = (currentX === this.ballRightSide) ? this.ballLeftSide : this.ballRightSide;
+    var y = f(x);
 
-    this.ball[0].x = x;
-    this.ball[0].y = y;
-    return {x: x, y: y};
+    // check for collisions
+    var minDistance = 9999999;
+    _.each(this.ballCollisionSegments, function(e) {
+        var intersection = SimpleMath.getIntersection(segment.a, segment.b, e[0], e[1]);
+
+        console.log('i e', intersection, e);
+
+        if ( !!intersection && intersection[0] != currentX && intersection[1] != currentY) {
+            var dist = SimpleMath.distance({x: currentX, y: currentY}, {x: intersection[0], y: intersection[1] });
+            // console.log('d', dist);
+            if (dist < minDistance) {
+                minDistance = dist;
+                destX = intersection[0];
+                destY = intersection[1];
+            }
+        }
+    });
+
+    console.log('s', segment);
+    console.log('x', x, destX);
+    console.log('y', y, destY);
+
+    this.moveCount++;
+    this.ball[0].x = destX;
+    this.ball[0].y = destY;
+    return {
+        x: destX,
+        y: destY
+    };
 };
 
 var gameBoard = new Board();
--- app/js/simpleMath.js
+++ app/js/simpleMath.js
@@ -1,7 +1,74 @@
-function toRadians (angle) {
-  return angle * (Math.PI / 180);
+function toRadians(angle) {
+    return angle * (Math.PI / 180);
+}
+
+function distance(a, b) {
+    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+}
+
+function getF(k, b) {
+    return function(x) {
+        return k * x + b;
+    };
+}
+
+var linear = {
+    getSlope: function(x1, y1, x2, y2) {
+        if (x1 == x2) return false;
+        return (y1 - y2) / (x1 - x2);
+    },
+    getYInt: function(x1, y1, x2, y2) {
+        if (x1 === x2) return y1 === 0 ? 0 : false;
+        if (y1 === y2) return y1;
+        return y1 - this.getSlope(x1, y1, x2, y2) * x1;
+    },
+    getXInt: function(x1, y1, x2, y2) {
+        var slope;
+        if (y1 === y2) return x1 === 0 ? 0 : false;
+        if (x1 === x2) return x1;
+        return (-1 * ((slope = this.getSlope(x1, y1, x2, y2)) * x1 - y1)) / slope;
+    },
+    getInt: function(x11, y11, x12, y12, x21, y21, x22, y22) {
+        var slope1, slope2, yint1, yint2, intx, inty;
+
+        // check if either of the points are the same. if so that's our intersection
+        if (x11 == x21 && y11 == y21) return [x11, y11];
+        if (x12 == x22 && y12 == y22) return [x12, y22];
+
+        // get slope: (y1 - y1) / (x1 - x2)
+        slope1 = this.getSlope(x11, y11, x12, y12);
+        slope2 = this.getSlope(x21, y21, x22, y22);
+
+        // if both lines have the same slope, they are paralell; never touch.
+        if (slope1 === slope2) return false;
+
+        // get y-intersection: y - slope * x
+        yint1 = this.getYInt(x11, y11, x12, y12);
+        yint2 = this.getYInt(x21, y21, x22, y22);
+
+        // check to see if both lines have the same yintcept, and if so, return the point
+        if (yint1 === yint2) return yint1 === false ? false : [0, yint1];
+
+        // if one of the lines doesn't have a slope:
+        if (slope1 === false) return [x21, slope2 * x21 + yint2];
+        if (slope2 === false) return [x11, slope1 * x11 + yint1];
+
+        //if both lines have a slop and y intercept, calulate the x-intercept:
+        // (slope1 * x1 + yint1 - yint2) / slope2;
+        intx = (slope1 * x11 + yint1 - yint2) / slope2;
+
+        // calculate the y-intercept, and return an array:
+        return [intx, slope1 * intx + yint1];
+    }
+};
+
+function getIntersection(a, b, c, d) {
+    return linear.getInt(a.x, a.y, b.x, b.y, c.x, c.y, d.x, d.y);
 }
 
 export default {
-    toRadians: toRadians
+    toRadians: toRadians,
+    distance: distance,
+    getIntersection: getIntersection,
+    getF: getF
 };
